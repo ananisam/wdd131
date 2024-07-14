bfa556e74f74f121df3888fc70a7ed10dc6fd7dc
ananisam <sam23008@byui.edu>
2024-07-14 07:33:36 +0100

    update

--- scripts/place.js
+++ scripts/place.js
@@ -1,19 +1,19 @@
 // Declaring constants
 const temp = document.querySelector("#temperature");
-const condit = document.querySelector("#condition");
+const cond = document.querySelector("#condition");
 const wind = document.querySelector("#wind");
 const windchill = document.querySelector("#windchill");
 
 const temperature = 32;
 const condition = "Sunny";
-const windSpeed = 9;
+const windSpeed = 5;
 
 function calculateWindChill(temperature, windSpeed) {
     return (13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature) * Math.pow(windSpeed, 0.16)).toFixed(2);
 }
 
 temp.textContent = temperature;
-condit.textContent = condition;
+cond.textContent = condition;
 wind.textContent = windSpeed;
 
 // Setting the decisions
